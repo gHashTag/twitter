37724f7172f14e1862ffd004c124bafa1429be2c
playra <raoffonom@icloud.com>
2017-09-20 17:48:26 +0300

    bug

--- mobile/src/store.js
+++ mobile/src/store.js
@@ -1,15 +1,22 @@
 /* eslint-disable no-param-reassign */
+
 import { createStore, applyMiddleware } from 'redux'
 import { AsyncStorage } from 'react-native'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import ApolloClient, { createNetworkInterface } from 'apollo-client'
 import thunk from 'redux-thunk'
-import logger from 'redux-logger'
+import { createLogger } from 'redux-logger'
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
 
 import reducers from './reducers'
 
 const networkInterface = createNetworkInterface({
   uri: 'http://localhost:3000/graphql',
+})
+
+const wsClient = new SubscriptionClient('ws://localhost:3000/subscriptions', {
+  reconnect: true,
+  connectionParams: {}
 })
 
 networkInterface.use([{
@@ -18,22 +25,28 @@ networkInterface.use([{
       req.options.headers = {}
     }
     try {
-      const token = await AsyncStorage.getItem('@twitteryourtubeclone') 
+      const token = await AsyncStorage.getItem('@twitteryoutubeclone')
       if (token != null) {
-        req.options.headers.authorization = `Bearer ${token} || null`
-      }  
-    } catch (e) {
-      throw e
+        req.options.headers.authorization = `Bearer ${token}` || null
+      }
+    } catch (error) {
+      throw error
     }
+
     return next()
   }
 }])
 
-export const client = new ApolloClient({
+const networkInterfaceWithSubs = addGraphQLSubscriptions(
   networkInterface,
+  wsClient
+)
+
+export const client = new ApolloClient({
+  networkInterface: networkInterfaceWithSubs
 })
 
-const middlewares = [client.middleware(), thunk, logger]
+const middlewares = [client.middleware(), thunk, createLogger()]
 
 export const store = createStore(
   reducers(client),
